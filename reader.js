cb4fffb45663332a515ca67a626146b5f7693730
afamumejiaku <45050314+afamumejiaku@users.noreply.github.com>
2020-12-09 14:09:22 -0600

    Add files via upload

--- reader.js
+++ reader.js
@@ -3,14 +3,14 @@ var stopwords = new Set(
     ','
   )
 )
-
+var data
 async function getData(input) {
   input = input.split('\\')[2]
   console.log(input)
 
   let tweets = []
   const response = await fetch(input)
-  const data = await response.text()
+  data = await response.text()
   const rows = data.split('\n').slice(1)
   var wordDict = {}
 
@@ -53,7 +53,6 @@ async function getData(input) {
   for (dict in wordDict) {
     outPut.push(wordDict[dict])
   }
-
   return outPut
 }
 
@@ -66,3 +65,53 @@ function getColor(value) {
     return 'red'
   }
 }
+
+
+
+
+async function originalData() {
+    // Reading the file using default 
+  const data3 = data 
+  const array1 = data3.split('\r')        // split it in an array 
+  // All the rows of the CSV will be converted to JSON objects which will be added to result in an array 
+  let result = [];  
+  // The array[0] contains all the  header columns so we store them  in headers array 
+  let headers = array1[0].split(",") 
+  // Since headers are separated, we need to traverse remaining n-1 rows.  
+  for (let i = 1; i < array1.length - 1; i++) { 
+   let obj = {} 
+  // Create an empty object to later add values of the current row to it Declare string str as current array 
+  // value to change the delimiter and store the generated string in a new string s 
+  let str = array1[i] 
+   let s = ''
+  // By Default, we get the comma seprated values of a cell in quotes " " so we use flag to keep track of quotes and  
+  // split the string accordingly. If we encounter opening quote (") then we keep commas as it is otherwise 
+  // we replace them with pipe | We keep adding the characters we traverse to a String s 
+  let flag = 0 
+  for (let ch of str) { 
+    if (ch === '"' && flag === 0) { 
+      flag = 1 
+    } 
+    else if (ch === '"' && flag == 1) flag = 0 
+    if (ch === ',' && flag === 0) ch = '|'
+    if (ch !== '"') s += ch 
+  } 
+  // Split the string using pipe delimiter | and store the values in a properties array 
+  let properties = s.split("|") 
+  // For each header, if the value contains multiple comma separated data, then we 
+  // store it in the form of array otherwise directly the value is stored 
+  for (let j in headers) { 
+    if (properties[j].includes(",")) { 
+      obj[headers[j]] = properties[j] 
+        .split(", ").map(item => item.trim()) 
+    } 
+    else obj[headers[j]] = properties[j] 
+  } 
+  // Add the generated object to our result array  
+  result.push(obj) 
+} 
+// Convert the resultant array to json and  generate the JSON output file. 
+let json = JSON.stringify(result); 
+localStorage.setItem('output.json', json); 
+
+return(result)}
